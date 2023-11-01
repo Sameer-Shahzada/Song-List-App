91d36ad8cf4d7532acbbba9e1a08301229f8ca14
Sameer-Shahzada <md.shahzada1606@gmail.com>
2023-11-02 00:41:40 +0530

    Update - phone number validation - add +91 in textfield - otp input field validation - resend otp api & set otp on calling of resend api

--- src/components/OTPVerification.jsx
+++ src/components/OTPVerification.jsx
@@ -4,9 +4,8 @@ import { Box, Typography, TextField, Button, Link } from "@mui/material";
 import SongList from "./SongList";
 
 const OTPVerification = ({ phoneNumber, requestId }) => {
-
   const [otp, setOtp] = useState(Array(4).fill(""));
-  const [isOTPVerified, setIsOTPVerified] = useState(false)
+  const [isOTPVerified, setIsOTPVerified] = useState(false);
   const inputRefs = new Array(4);
 
   const handleInput = (e, index) => {
@@ -33,100 +32,127 @@ const OTPVerification = ({ phoneNumber, requestId }) => {
     // console.table(requestId , phoneNumber);
     // Build the OTP from the array of input values
     const enteredOTP = otp.join("");
-    console.log('received otp - ', enteredOTP)
-    let url = 'https://dev.api.goongoonalo.com/v1/auth/verify_otp';
+    console.log("received otp - ", enteredOTP);
+    let url = "https://dev.api.goongoonalo.com/v1/auth/verify_otp";
     let data = {
       requestId: requestId,
-      phoneNumber: '+91' + phoneNumber,
+      phoneNumber: "+91" + phoneNumber,
       otp: enteredOTP,
-    }
-    axios.post(url, data)
-      .then(response => {
-        console.log(response)
+    };
+    axios
+      .post(url, data)
+      .then((response) => {
+        console.log(response);
         if (response.status === 200) {
           setIsOTPVerified(true); // Mark OTP as verified
         }
-        console.log(response.status)
+        console.log(response.status);
       })
-    // .catch(error => console.log('verify error occurs...'));
+      .catch((error) => {
+        if (otp.every((value) => value === "")) {
+          console.log("Please enter OTP...");
+        } else if (otp.every((value) => value !== enteredOTP)) {
+          console.log("Incorrect OTP...");
+        } else {
+          console.log("something went wrong");
+        }
+      });
+  };
 
-  }
+  const resendOTP = () => {
+    let url = "https://dev.api.goongoonalo.com/v1/auth/login";
+    let data = {
+      phoneNumber: "+91" + phoneNumber,
+    };
 
-  return (
-    isOTPVerified ? <SongList /> : (
-      <>
+    console.log(data);
+    axios
+      .post(url, data)
+      .then((response) => {
+        // setRequestId(response.data.requestId);
+        setOtp(["5", "6", "7", "8"]);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log('resend')
+       
+      });
+  };
+
+  return isOTPVerified ? (
+    <SongList />
+  ) : (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
+            width: "414px",
+            height: "334px",
+            // border: 1,
           }}
         >
-          <Box
-            sx={{
-              width: "414px",
-              height: "334px",
-              // border: 1,
-            }}
-          >
-            <Typography variant="h1" sx={{ fontSize: "38px", color: "#552583" }}>
-              OTP Verification
-            </Typography>
-            <Box sx={{ mt: 2, mb: 2.5 }}>
-              <Typography variant="body1">
-                We have sent an OTP to +919548337884. Please enter the code
-                received to verify.
-              </Typography>
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
-              {otp.map((value, index) => {
-                console.log('value', value, index)
-                return (
-                  <TextField
-                    key={index}
-                    size="large"
-                    variant="outlined"
-                    value={value}
-                    onChange={(e) => {
-                      handleInput(e, index);
-                      // setReceivedOtp(e.target.value);
-                    }}
-                    inputRef={(el) => (inputRefs[index] = el)}
-                    inputProps={{ style: { textAlign: "center" } }}
-                    sx={{ width: "80px" }}
-                  />
-                );
-              })}
-            </Box>
-
-            <Box sx={{ my: 3 }}>
-              <Button
-                variant="contained"
-                sx={{
-                  width: "100%",
-                  backgroundColor: "#552583",
-                  color: "white",
-                  "&:hover": {
-                    backgroundColor: "#552583",
-                  },
-                }}
-                onClick={handleVerify}
-              >
-                Verify
-              </Button>
-            </Box>
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <Link>Resend OTP</Link>
-            </Box>
-            <Typography sx={{ display: "flex", justifyContent: "center", my: 2 }}>
-              <Link>Use another number</Link>
+          <Typography variant="h1" sx={{ fontSize: "38px", color: "#552583" }}>
+            OTP Verification
+          </Typography>
+          <Box sx={{ mt: 2, mb: 2.5 }}>
+            <Typography variant="body1">
+              We have sent an OTP to +919548337884. Please enter the code
+              received to verify.
             </Typography>
           </Box>
-        </Box>
-      </>
-    )
+          <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
+            {otp.map((value, index) => {
+              console.log("value", value, index);
+              return (
+                <TextField
+                  key={index}
+                  size="large"
+                  variant="outlined"
+                  value={value}
+                  onChange={(e) => {
+                    handleInput(e, index);
+                    // setReceivedOtp(e.target.value);
+                  }}
+                  inputRef={(el) => (inputRefs[index] = el)}
+                  inputProps={{ style: { textAlign: "center" } }}
+                  sx={{ width: "80px" }}
+                />
+              );
+            })}
+          </Box>
 
+          <Box sx={{ my: 3 }}>
+            <Button
+              variant="contained"
+              sx={{
+                width: "100%",
+                backgroundColor: "#552583",
+                color: "white",
+                "&:hover": {
+                  backgroundColor: "#552583",
+                },
+              }}
+              onClick={handleVerify}
+            >
+              Verify
+            </Button>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Link onClick={resendOTP} href="#">Resend OTP</Link>
+          </Box>
+          <Typography sx={{ display: "flex", justifyContent: "center", my: 2 }}>
+            <Link href="#">Use another number</Link>
+          </Typography>
+        </Box>
+      </Box>
+    </>
   );
 };
 
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,95 +1,112 @@
 import { React, useState } from "react";
 import axios from "axios";
-import { Box, Typography, TextField, Button , InputAdornment  } from "@mui/material";
-import OTPVerification from './OTPVerification'
-import IndiaFlag from '../images/indiaflag25.png';
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  InputAdornment,
+} from "@mui/material";
+import OTPVerification from "./OTPVerification";
+import IndiaFlag from "../images/indiaflag25.png";
 const SignIn = () => {
-
-  const [phoneNumber, setPhoneNumber] = useState('')
-  const [requestId, setRequestId] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [requestId, setRequestId] = useState("");
 
   const handleSignIn = () => {
     let url = "https://dev.api.goongoonalo.com/v1/auth/login";
     let data = {
-      "phoneNumber": '+91' + phoneNumber,
-    }
-    console.log(data)
-    axios.post(url, data)
+      phoneNumber: "+91" + phoneNumber,
+    };
+    
+    console.log(data);
+    axios
+      .post(url, data)
       .then((response) => {
         setRequestId(response.data.requestId);
         console.log(response);
       })
-      .catch((error) => console.log("Login API Error"));
+      .catch((error) => {
+        if (phoneNumber.length > 10) {
+          console.log("Phone number is more than 10 digits");
+        } else if(phoneNumber.length < 10) {
+          console.log("Phone number is less than 10 digits");
+        } else {
+          console.log('Login API error...');
+        }
+      });
   };
 
-  return (
-    requestId ? <OTPVerification requestId={requestId} phoneNumber={phoneNumber} /> : (
-      <>
+  return requestId ? (
+    <OTPVerification requestId={requestId} phoneNumber={phoneNumber} />
+  ) : (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+        }}
+      >
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            minHeight: "100vh",
+            width: "414px",
+            height: "238px",
           }}
         >
-          <Box
-            sx={{
-              width: "414px",
-              height: "238px",
-            }}
-          >
-            <Typography variant="h1" sx={{ fontSize: "38px", color: "#552583" }}>
-              Sign In
+          <Typography variant="h1" sx={{ fontSize: "38px", color: "#552583" }}>
+            Sign In
+          </Typography>
+          <Box sx={{ mt: 2, mb: 2.5 }}>
+            <Typography variant="body1">
+              Please enter your number to login. We will send an OTP to verify
+              your number.
             </Typography>
-            <Box sx={{ mt: 2, mb: 2.5 }}>
-              <Typography variant="body1">
-                Please enter your number to login. We will send an OTP to verify
-                your number.
-              </Typography>
-            </Box>
-            <Box>
-              <TextField
-                size="small"
-                variant="outlined"
-                type="number"
-                value={phoneNumber}
-                placeholder="Phone Number"
-                onChange={(e) => setPhoneNumber(e.target.value)}
-                label="PHONE NUMBER"
-                sx={{ width: "100%" }}
-                // Add InputAdornment for the India flag
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start" sx={{py:'8px'}}>
-                      <img src={IndiaFlag} alt="India Flag" />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Box>
+          </Box>
+          <Box>
+            <TextField
+              size="small"
+              variant="outlined"
+              // type="number"
+              value={phoneNumber.replace(/[^0-9]/g,"")}
+              placeholder="Phone Number"
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              label="PHONE NUMBER"
+              sx={{ width: "100%" }}
+              // Add InputAdornment for the India flag
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start" sx={{ py: "8px" }}>
+                    <img src={IndiaFlag} alt="India Flag" />
+                    <Typography variant="body1" sx={{ paddingLeft: 1 }}>
+                      +91
+                    </Typography>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Box>
 
-            <Box sx={{ mt: 3 }}>
-              <Button
-                variant="contained"
-                sx={{
-                  width: "100%",
+          <Box sx={{ mt: 3 }}>
+            <Button
+              variant="contained"
+              sx={{
+                width: "100%",
+                backgroundColor: "#552583",
+                color: "white",
+                "&:hover": {
                   backgroundColor: "#552583",
-                  color: "white",
-                  "&:hover": {
-                    backgroundColor: "#552583",
-                  },
-                }}
-                onClick={handleSignIn}
-              >
-                Signin
-              </Button>
-            </Box>
+                },
+              }}
+              onClick={handleSignIn}
+            >
+              Signin
+            </Button>
           </Box>
         </Box>
-      </>
-    )
-
+      </Box>
+    </>
   );
 };
 
