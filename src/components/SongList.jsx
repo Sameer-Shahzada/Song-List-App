7d4ec95ac2905d5fc4f83af9257d4b0abc22654d
Sameer-Shahzada <md.shahzada1606@gmail.com>
2023-11-03 01:08:41 +0530

    song-list page initiated

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,32 +1,67 @@
-import React from 'react'
-import { Box, Typography, } from '@mui/material'
-
+import React from "react";
+import songIcon from "../images/songIcon.svg";
+import logoutIcon from '../images/logoutIcon.svg';
+import { Box, Typography } from "@mui/material";
 
 const SongList = () => {
-    return (
-        <Box>
-            <Box sx={{
-                height: '100vh',
-                width: '256px',
-                backgroundColor: 'gold',
-                // border:1
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        width: "256px",
+        }}>
 
-            }}>
-                <Box sx={{
-                    border: 1,
-                }}>
-                    <Typography variant='body1' sx={{
-                        color: '#552583',
-                        fontSize: '36px', display: 'flex', justifyContent: 'center'
-                    }}>
-                        Logo
-                    </Typography>
+      <Box
+        sx={{
+          border: 1,
+          mb: 4,
+        }}
+      >
+        <Typography
+          variant="body1"
+          sx={{
+            color: "#552583",
+            fontSize: "36px",
+            display: "flex",
+            justifyContent: "center",
+            // fontFamily: 'Roboto',
+            fontWeight: 700,
+            my: 1.5,
+          }}
+        >
+          Logo
+        </Typography>
+      </Box>
 
-                </Box>
-
-            </Box>
+      <Box
+        sx={{
+          display: "flex",
+          backgroundColor: "#E6F7FF",
+          py: 0.5,
+          borderRight: 3,
+          borderColor: "#1890FF",
+        }}
+      >
+        <Box sx={{ marginLeft: 3, marginRight: 2 }}>
+          <img src={songIcon} alt="songIcon" />
         </Box>
-    )
-}
 
-export default SongList
+        <Typography variant="body1" sx={{ color: "#1890FF", fontSize: "14px" }}>
+          Songs
+        </Typography>
+      </Box>
+
+      <Box sx={{ display:'flex', border:1, py: 0.5, position:'fixed', bottom: 2, width:'255px'  }}>
+        <Box sx={{ marginLeft: 3, marginRight: 2 }}>
+          <img src={logoutIcon} alt="logoutIcon" />
+        </Box>
+
+        <Typography variant="body1" sx={{ fontSize: "14px"}}>
+          Logout
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default SongList;
